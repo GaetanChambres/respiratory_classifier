a7c6825286a6e7b2599278e4eda7e8db6caa7944
GaetanChambres <gaetan.chambres@u-bordeaux.fr>
2019-10-18 17:51:43 +0200

    new files for the horse experiment, splitting computes the audio for cycles, must run no_symptoms_preprocess to compute ft and labelising gives the labels... then the files are usable with no_symptoms_classif

---
+++ src/labelising_no_symptoms.py
@@ -0,0 +1,111 @@
+#
+#
+#
+# ce fichier trie les cycles respiratoires d'un dossier donné en identifiant
+# les cycles sains sans symptomes (label=0) des cycles pathologiques sans symptomes (label=1)
+# On sauve une liste de listes qui contienent les données audio des cycles suivi de leur label
+# on exporte cette liste sous format pickle
+#
+#
+#
+
+
+import os
+import sys
+import librosa
+import numpy as np
+import pickle
+from progressbar import ProgressBar
+
+pbar = ProgressBar()
+
+out_csv_file = "experiments/data/horse_v3/test.csv"
+
+
+# Function that return an alphabetically ordered list
+# of the files included in a given directory
+###########################################################################
+def ordering_files(directory):
+    return(sorted(os.listdir(directory)))
+##########################################################################
+
+
+
+cycles_path = "experiments/data/horse_v3/test/"
+csv_file = "experiments/data/horse_v3/info_test_ch.csv"
+
+files_list = ordering_files(cycles_path)
+
+pathologies = np.loadtxt(csv_file,delimiter = ',',skiprows = 0,usecols=range(3,4))
+print(pathologies) #list of int from 0 to 6
+
+crackles = crackles = np.loadtxt(csv_file,delimiter = ',',skiprows = 0,usecols=range(4,5))
+print(crackles) #list of int from 0 to 1
+
+wheezes = np.loadtxt(csv_file,delimiter = ',',skiprows = 0,usecols=range(5,6))
+print(wheezes) #list of int from 0 to 1
+
+cpt=0
+cpt0=0
+cpt1=0
+mylist=[]
+labels =[]
+
+# creation of the final list, with len(files_list) the size (the number of files)
+# for r in range(0,len(files_list)):
+#     mylist.append(0)
+#     labels.append(0)
+# print(len(mylist))
+# print(mylist)
+
+out_file = open(out_csv_file, "w")
+
+from shutil import copyfile
+
+for f1 in pbar(files_list): #pbar is progressbar; only visual
+
+    y, sr = librosa.load(cycles_path+f1) # open the audio file
+    # print("sampling rate = ",sr)
+
+    # print("label =")
+    # print(pathologies[cpt])
+    # print("values =")
+    # print(y)
+
+    if(pathologies[cpt]==7): #if pathology is healthy
+        if ( (int(crackles[cpt])==0) and (int(wheezes[cpt])==0) ): #if there is NO crackle AND NO wheeze
+            # print(cpt)
+            cpt0+=1 #add 1 to the number of healthy records whithout symptoms
+            # a = np.append(y,pathologies[cpt]) #add patho label after the audio values
+            a = np.append(y,0) #add 0 to labelize healthy record
+
+            # mylist[cpt]=a #save the final array into a list of arrays
+            mylist.append(a)
+            # labels[cpt] = 0
+            out_file.write(f1[:-4]+",0\n")
+        else:
+            print("encoutered a cycle healthy with symptoms")
+    else:
+        if ( (int(crackles[cpt])==0) and (int(wheezes[cpt])==0) ): #if pathology IS NOT healthy, but no symptoms
+            cpt1+=1 #add 1 to the number of pathological records whithout symptoms
+            # a = np.append(y,pathologies[cpt]) #add patho label after the audio values
+            a = np.append(y,1) #add 1 to labelize healthy record
+
+            # mylist[cpt]=a #save the final array into a list of arrays
+            mylist.append(a)
+            # labels[cpt]=1
+            out_file.write(f1[:-4]+",1\n")
+
+    cpt+=1
+
+print("healthy cycles without symptoms",cpt0)
+print("pathological cycles whithout symptoms : ",cpt1)
+print("total interesting values should be : ",cpt0+cpt1)
+print("total is actually : ",len(mylist)) #should give the number of files
+# print(len(labels))
+# print(len(mylist[0]))
+
+# pickle.dump(mylist,open(pickle_file,'wb'))
+# pickle.dump(labels,open('pickles/train_labels','wb'))
+
+print("Finished")
---
+++ src/splitting.py
@@ -0,0 +1,102 @@
+
+#  ██  ██      ███████ ██████  ██      ██ ████████ ████████ ██ ███    ██  ██████
+# ████████     ██      ██   ██ ██      ██    ██       ██    ██ ████   ██ ██
+#  ██  ██      ███████ ██████  ██      ██    ██       ██    ██ ██ ██  ██ ██   ███
+# ████████          ██ ██      ██      ██    ██       ██    ██ ██  ██ ██ ██    ██
+#  ██  ██      ███████ ██      ███████ ██    ██       ██    ██ ██   ████  ██████
+
+###########################################################################
+# When using this script, you will take all the resp. records in a given directory (argument 1)
+# and the given input csvfile (argument 2) and will
+# split all the records into audio samples for each resp cycles
+#
+# So in the input folder, this script will add :
+# -- a folder named "splitted_into_cycles" containing audio files
+# -- each audio file in this folder corresponds to a resp. cycle
+###########################################################################
+
+import sys
+import os
+import csv
+import pydub
+from progressbar import ProgressBar
+import utilities.parsing_tools as prsg
+
+###########################################################################
+# Function that splits full resp. records into resp. cycles
+# It takes a input directory, a csv file and an output directory
+#
+# All the sub-samples are saved in the given output directory
+###########################################################################
+def split_record_in_cycle(dir,file_csv,output_dir) :
+    pbar = ProgressBar()
+    lines = prsg.nb_lines(file_csv)
+    with open(file_csv, newline='') as csvfile:
+        data = list(csv.reader(csvfile))
+    print(data)
+    cpt=0
+    for i in range(0,len(data)):
+        filename = save_file_name = data[i][0]
+
+        print("Processed record = "+data[i][0]+" nb cycle = "+str(cpt))
+        myaudio = pydub.AudioSegment.from_wav(dir+data[i][0]+".wav")
+        chunk_data = myaudio[int(float(data[i][1])*1000):int(float(data[i][2])*1000)]
+        saved_file = (output_dir+save_file_name+"_"+"{0:0=2d}".format(cpt)+".wav")
+        # print("saved cycle name = "+saved_file)
+        chunk_data.export(saved_file, format="wav")
+        # i+=1
+        cpt+=1
+        # if i == lines:
+            # break
+    return cpt
+    import sys
+    sys.exit()
+    input_dir = prsg.ordering_files(dir)
+    # print(input_dir)
+    i=0
+    for filename in pbar(input_dir):
+        if(filename.endswith('.wav')):
+            cpt=1
+            save_file_name = filename[:-4]
+            print()
+            filename = data[i][0]
+            while data[i][0] == save_file_name:
+                print("Processed record = "+data[i][0]+" nb cycle = "+str(cpt))
+                myaudio = pydub.AudioSegment.from_wav(dir+data[i][0]+".wav")
+                chunk_data = myaudio[int(float(data[i][1])*1000):int(float(data[i][2])*1000)]
+                saved_file = (output_dir+save_file_name+"_"+"{0:0=2d}".format(cpt)+".wav")
+                # print("saved cycle name = "+saved_file)
+                chunk_data.export(saved_file, format="wav")
+                i+=1
+                cpt+=1
+                if i == lines:
+                    break
+    return i
+
+###########################################################################
+
+#  ██  ██      ███    ███  █████  ██ ███    ██
+# ████████     ████  ████ ██   ██ ██ ████   ██
+#  ██  ██      ██ ████ ██ ███████ ██ ██ ██  ██
+# ████████     ██  ██  ██ ██   ██ ██ ██  ██ ██
+#  ██  ██      ██      ██ ██   ██ ██ ██   ████
+
+###########################################################################
+
+arguments = sys.argv
+# if len(arguments) != 3:
+#     print("Usage Error : wrong argument number")
+#     print("Usage : \n arg1 = path_to_data_folder -with wav and txt inside- \n arg2 = path_to_csv_info_file")
+#     sys.exit()
+directory = arguments[1]
+csv_info = arguments[2]
+sample_save_place = arguments[3]
+# sample_save_place = directory+"splitted_into_cycles/"
+os.makedirs(sample_save_place, exist_ok=True)
+#
+total = split_record_in_cycle(directory,csv_info,sample_save_place)
+# print()
+print("All files splitted :")
+print(str(total)+" samples saved in "+sample_save_place )
+
+###########################################################################
